c024e2c14d710840df1e972a7bcfa47d03e914af
rshivam973 <rshivam973@gmail.com>
2023-06-03 02:20:33 +0530

    new comit

--- src/components/helper.js
+++ src/components/helper.js
@@ -1,2 +1,2 @@
 // export const BASE_URL="http://localhost:5000"
-export const BASE_URL="https://facebookbackend-f6ei.onrender.com" ;
+export const BASE_URL="https://facebookbackend-f6ei.onrender.com";
